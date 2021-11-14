55fb9538a5593aae3d8925f8c0dc0e4c61177a36
NoncreativeUsername <71220035+NoncreativeUsername@users.noreply.github.com>
2021-11-14 11:01:36 -0600

    Update EE428.ino

--- EE428.ino
+++ EE428.ino
@@ -1,16 +1,39 @@
 #include <PS2X_lib.h>
 #include <Servo.h>
+#include <SharpIR.h>
+#include <Math.h>
+#include <MedianFilterLib2.h>
+
+// Define model and input pin:
+#define IRPinR A0
+#define IRPinL A5
+#define model 430
+
+// Create variable to store the distance:
+float distanceR_cm;  // distance on left sensor
+float distanceL_cm;  // destance on right sensor
+
+float pie = 3.1415926535897932384626433832795;
+float sensorDist; // distance between sensors
+
+float angle_wall;    // angle with wall
+float angle_adj;     // angle of adjustment required to be perpendicular to wall
+
+float medianR;
+float medianL;
+
+// Create a new instance of the SharpIR class:
+SharpIR IrRSensor = SharpIR(IRPinR, model);
+SharpIR IrLSensor = SharpIR(IRPinL, model);
+
+int ws = 200;  // window size for the median finder
 
 PS2X ps2x;
- 
-//right now, the library does NOT support hot-pluggable controllers, meaning 
-//you must always either restart your Arduino after you connect the controller, 
-//or call config_gamepad(pins) again after connecting the controller.
 
 //motor pwm values
 int percentL = 0;   //percent of left motors
 int percentR = 0;   //from -100 to 100
-int dir = 0;        //int input will determine robot direction
+//int dir = 0;        //int input will determine robot direction
 int pins[] = {6, 8}; //the PWM signal output pins
 
 const int arraySize = sizeof(pins)/sizeof(int);
@@ -21,13 +44,11 @@ int error = 0;
 byte type = 0;
 byte vibrate = 0;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   
   for (int i=0; i<arraySize; i++)
     controllers[i].attach(pins[i]); //associate the object to a pin
-    
-
   error = ps2x.config_gamepad(13,11,10,12, true, true);   //GamePad(clock, command, attention, data, Pressures?, Rumble?) 
   if(error == 0){
     Serial.println("Found Controller, configured successful");
@@ -56,7 +77,16 @@ void setup(){
     delay(500);
 }
 
-void loop(){
+void loop() {
+  sensorDist = 4.5; // inches between IR sensor
+
+  // for loop for finding the medians of the IR data sets
+  for (int i = 0; i < ws+1; i++){
+    MedianFilter2<float> medianFilter2(ws);
+
+  // Get a distance measurement and store it as distance_cm:
+  distanceR_cm = IrRSensor.distance();
+  
   int PWMvalueL = percentL * 5 + 1500; //scale up to 1000-2000
   int PWMvalueR = percentR * 5 + 1500; //scale up to 1000-2000
 
@@ -64,76 +94,5 @@ void loop(){
   controllers[0].writeMicroseconds(PWMvalueR);
   controllers[1].writeMicroseconds(PWMvalueR);
   
-  /* You must Read Gamepad to get new values
-  Read GamePad and set vibration values
-  ps2x.read_gamepad(small motor on/off, larger motor strenght from 0-255)
-  if you don't enable the rumble, use ps2x.read_gamepad(); with no values
-  you should call this at least once a second
-  */
-if(error == 1) 
- return; 
-if(type == 2){      //guitar hero controler
-  ps2x.read_gamepad();          //read controller 
-  
+  delay(50);
 }
-else { //DualShock Controller
-  ps2x.read_gamepad(false, vibrate);          //read controller and set large motor to spin at 'vibrate' speed
-  if(ps2x.Button(PSB_START))                   //will be TRUE as long as button is pressed
-    Serial.println("Start is being held");
-  if(ps2x.Button(PSB_SELECT))
-    Serial.println("Select is being held");
-  if(ps2x.Button(PSB_PAD_UP))                 //will be TRUE as long as button is pressed 
-  {         
-    Serial.print("Up held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
-  }
-  if(ps2x.Button(PSB_PAD_RIGHT))
-  {
-    Serial.print("Right held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
-  }
-  if(ps2x.Button(PSB_PAD_LEFT))
-  {
-    Serial.print("LEFT held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
-  }
-  if(ps2x.Button(PSB_PAD_DOWN))
-  {
-    Serial.print("DOWN held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
-  }   
-  vibrate = ps2x.Analog(PSAB_BLUE);        //this will set the large motor vibrate speed based on 
-                                             //how hard you press the blue (X) button    
-  if (ps2x.NewButtonState())               //will be TRUE if any button changes state (on to off, or off to on)
-  {
-    if(ps2x.Button(PSB_L3))
-      Serial.println("L3 pressed");
-    if(ps2x.Button(PSB_R3))
-      Serial.println("R3 pressed");
-    if(ps2x.Button(PSB_L2))
-      Serial.println("L2 pressed");
-    if(ps2x.Button(PSB_R2))
-      Serial.println("R2 pressed");
-    if(ps2x.Button(PSB_GREEN))
-      Serial.println("Triangle pressed");
-   }   
-   if(ps2x.ButtonPressed(PSB_RED))             //will be TRUE if button was JUST pressed
-      Serial.println("Circle just pressed");
-   if(ps2x.ButtonReleased(PSB_PINK))             //will be TRUE if button was JUST released
-      Serial.println("Square just released");     
-   if(ps2x.NewButtonState(PSB_BLUE))            //will be TRUE if button was JUST pressed OR released
-      Serial.println("X just changed");    
-   if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1)) // print stick values if either is TRUE
-   {
-      Serial.print("Stick Values:");
-      Serial.print(ps2x.Analog(PSS_LY), DEC); //Left stick, Y axis. Other options: LX, RY, RX  
-      Serial.print(",");
-      Serial.print(ps2x.Analog(PSS_LX), DEC); 
-      Serial.print(",");
-      Serial.print(ps2x.Analog(PSS_RY), DEC); 
-      Serial.print(",");
-      Serial.println(ps2x.Analog(PSS_RX), DEC); 
-   } 
-}
-delay(50);
-} 
