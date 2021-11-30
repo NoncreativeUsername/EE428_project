2919ca7cb5ce3d0da3c3f4af3a121fa78c0cacae
NoncreativeUsername <71220035+NoncreativeUsername@users.noreply.github.com>
2021-11-30 08:55:04 -0600

    Update EE428.ino

--- EE428.ino
+++ EE428.ino
@@ -1,48 +1,34 @@
+
 #include <Servo.h>
 #include <SharpIR.h>
 #include <Math.h>
 #include <MedianFilterLib2.h>
-
-// Define model and input pin:
-#define IRPinR A0
-#define IRPinL A5
-#define IRPinM A4
-#define model 430
 
 // Create variable to store the distance:
 float distanceR_cm;  // distance on left sensor
 float distanceL_cm;  // distance on right sensor
 float distanceM_cm;  // distance on middle sensor
 
-float pie = 3.1415926535897932384626433832795;
-float sensorDist; // distance between sensors
-
-float angle_wall;     // angle with wall
-float angle_adj;      // angle of adjustment required to be perpendicular to wall
-
 float medianR;        //right
 float medianL;        //left
 float medianM;        //middle
 
-byte percentL = 0;   //percent of left motors
-byte percentR = 0;   //from -100 to 100
 
 int PWMvalueL = 1500;
 int PWMvalueR = 1500;
 
-//direction to get closer to target
-bool forward = false;
-bool right = false;
-bool left = false;
 
 // Create a new instance of the SharpIR class:
-SharpIR IrRSensor = SharpIR(IRPinR, model);
-SharpIR IrLSensor = SharpIR(IRPinL, model);
-SharpIR IrMSensor = SharpIR(IRPinM, model);
+//SharpIR IrRSensor = SharpIR(IRPinR, 430);
+//SharpIR IrLSensor = SharpIR(IRPinL, 430);
+//SharpIR IrMSensor = SharpIR(A0, 1080);
 
 int ws = 200;  // window size for the median finder
 
 Servo servo1, motorR, motorL;
+float value1 = 0;
+float value2 = 0;
+float value3 = 0;
 
 void setup() {
   servo1.attach(9, 500, 2500);    //flipper servo
@@ -51,57 +37,79 @@ void setup() {
   motorL.attach(5); //associate the left to a pin
 
   Serial.begin (115200);
-  Serial.println (F("Ready!"));
 }
 
 void loop() {
-  
-  sensorDist = 4.5; // inches between IR sensor
 
   // for loop for finding the medians of the IR data sets
   //smoothes out random outliers
+  /*
   for (int i = 0; i < ws+1; i++)
   {
     MedianFilter2<float> medianFilter2(ws);
 
     // Get a distance measurement and store it as distance_cm:
-    distanceR_cm = IrRSensor.getDistance();
-    distanceL_cm = IrLSensor.getDistance();
-    distanceM_cm = IrMSensor.getDistance();
+    value1 = analogRead(A0);  //right
+    value2 = analogRead(A1);  //center
+    value3 = analogRead(A2);  //left
+    //distanceR_cm = IrRSensor.getDistance();
+    //distanceL_cm = IrLSensor.getDistance();
+    //distanceM_cm = IrMSensor.getDistance();
 
-    medianL = medianFilter2.AddValue(distanceL_cm); 
-    medianR = medianFilter2.AddValue(distanceR_cm); 
-    medianM = medianFilter2.AddValue(distanceM_cm);
-  }
+    medianL = medianFilter2.AddValue(value3); 
+    medianR = medianFilter2.AddValue(value1); 
+    medianM = medianFilter2.AddValue(value2);
+  }*/
 
   //may not need this, keep just in case
-  //angle_adj = atan((medianR/2.54 - medianL/2.54)/sensorDist)/pie*180; // find the angle of adjustment. converts cm to Inches before calculation, 
+  //angle_adj = atan((medianR/2.54 - medianL/2.54)/sensorDist)/pie*180; // find the angle of adjustment. converts cm to Inches before calculation,
+  value1 = analogRead(A0);  //right
+  value2 = analogRead(A1);  //center
+  value3 = analogRead(A2);  //left
+  Serial.print("R: ");
+  Serial.print(value1);
+  //value1 = analogRead(A0)*0.0048828125;
+  //distanceM_cm = 13*pow(value, -1);
+  Serial.print("  C: ");
+  Serial.print(value2);
+  Serial.print("  L: ");
+  Serial.println(value3);
 
-  
-  
-  if (!forward and !left and !right)    //no targets in range
-    seek_target();                      //find new target
-  else
-    follow();
+  if (value2 > 120)     //object to close stop
+  {
+    motorR.writeMicroseconds(1500);
+    motorL.writeMicroseconds(1500);
+  }
+  else if (value2 > 40)     //object to far, follow
+  {
+    motorR.writeMicroseconds(1475);
+    motorL.writeMicroseconds(1525);
+  }
+  else if (value3 > 120)    //object to the left, turn
+  {
+    motorR.writeMicroseconds(1525);
+    motorL.writeMicroseconds(1525);
+    /*use this if you need to slow it down
+    delay(50);
+    motorR.writeMicroseconds(1500);
+    motorL.writeMicroseconds(1500);
+    */
+  }
+  else if (value2 > 120)    //object to the right, turn
+  {
+    motorR.writeMicroseconds(1475);
+    motorL.writeMicroseconds(1475);
+    /*
+    delay(50);
+    motorR.writeMicroseconds(1500);
+    motorL.writeMicroseconds(1500);*/
+  }
+  else                      //object lost, search
+  {
+    motorR.writeMicroseconds(1475);
+    motorL.writeMicroseconds(1475);
+  }
 
-  
-  delay(50);
-}
 
-void seek_target()    //find something to follow
-{
-  //spin to the right slowly
-  motorR.writeMicroseconds(1475);   //motors are reversed from each other
-  motorL.writeMicroseconds(1475);   //each motor 5%
-}
-
-void follow()         //follow your target
-{
-  //scale motor % to a motor PWM value
-  PWMvalueL = percentL * 5 + 1500; //scale up to 1000-2000
-  PWMvalueR = percentR * 5 + 1500; //scale up to 1000-2000
-
-  //write PWM value to data pins
-  motorR.writeMicroseconds(PWMvalueR);
-  motorL.writeMicroseconds(PWMvalueL);
+  delay(50);    //unsure if delay is necessary
 }
